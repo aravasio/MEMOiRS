1576d1829c66a22fcaef56ec0c1b6bcece6179c4
Alejandro Ravasio <alejandroravasio@gmail.com>
2019-03-18 12:56:02 -0300

    Added User model with its dependent, self-contained models. Also added small comments on Album and User.

--- memoirs/memoirs/Models/Album.swift
+++ memoirs/memoirs/Models/Album.swift
@@ -8,8 +8,17 @@
 
 import Foundation
 
+/**
+ Model representation of a Photo Album.
+ 
+ - parameters:
+     - id: The album's ID
+     - userId: The album's owner ID
+     - title: The album's name.
+ */
 
 struct Album: Codable {
+    
     let id: Int!
     let userId: Int!
     let title: String!
---
+++ memoirs/memoirs/Models/User.swift
@@ -0,0 +1,46 @@
+//
+//  User.swift
+//  memoirs
+//
+//  Created by Alejandro Ravasio on 18/03/2019.
+//  Copyright © 2019 Alejandro Ravasio. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Model representation of a User.
+ */
+
+struct User: Codable {
+    //Address struct definition
+    struct Address: Codable {
+        struct Location: Codable {
+            let lat: String
+            let lng: String
+        }
+        
+        let street: String
+        let suite: String
+        let city: String
+        let zipcode: String
+        let location: Location
+    }
+    
+    //Company struct definition
+    struct Company: Codable {
+        let name: String
+        let catchPhrase: String
+        let bs: String
+    }
+    
+    //Properties
+    let id: Int!
+    let name: String!
+    let username: String!
+    let email: String!
+    let address: Address!
+    let phone: String
+    let website: String
+    let company: Company
+}
