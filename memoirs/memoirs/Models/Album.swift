625b01917d71a103f9aeb6c611a596fddffb052d
Alejandro Ravasio <alejandroravasio@gmail.com>
2019-03-18 11:37:39 -0300

    Added Album model

---
+++ memoirs/memoirs/Models/Album.swift
@@ -0,0 +1,16 @@
+//
+//  Album.swift
+//  memoirs
+//
+//  Created by Alejandro Ravasio on 18/03/2019.
+//  Copyright © 2019 Alejandro Ravasio. All rights reserved.
+//
+
+import Foundation
+
+
+struct Album: Codable {
+    let id: Int!
+    let userId: Int!
+    let title: String!
+}
