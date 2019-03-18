d2f2c09cd3fc707ce059463140b0c97f55968264
Alejandro Ravasio <alejandroravasio@gmail.com>
2019-03-18 16:31:16 -0300

    Removed some optional markers from User.

--- memoirs/memoirs/Models/User.swift
+++ memoirs/memoirs/Models/User.swift
@@ -24,7 +24,7 @@ struct User: Codable {
         let suite: String
         let city: String
         let zipcode: String
-        let location: Location
+        let geo: Location
     }
     
     //Company struct definition
@@ -35,11 +35,11 @@ struct User: Codable {
     }
     
     //Properties
-    let id: Int!
-    let name: String!
-    let username: String!
-    let email: String!
-    let address: Address!
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+    let address: Address
     let phone: String
     let website: String
     let company: Company
